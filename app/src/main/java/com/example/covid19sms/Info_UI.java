50c4607d9348ad84d10735d5943d7b8c0b53a2f9
XhinoMullaymeri <dai16019@uom.edu.gr>
2020-03-23 06:08:56 +0200

    Great progress on login ui and log in activity code.

---
+++ app/src/main/java/com/example/covid19sms/Info_UI.java
@@ -0,0 +1,117 @@
+package com.example.covid19sms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Info_UI extends AppCompatActivity {
+
+    private SharedPreferences sharedPref;
+    private String key_first_name = "com.example.covid19sms.first_name";
+    private String key_last_name = "com.example.covid19sms.last_name";
+    private String key_address = "com.example.covid19sms.address";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+
+        sharedPref = this.getSharedPreferences(
+                "com.example.covid19sms", Context.MODE_PRIVATE);
+
+
+        //sharedPref.edit().clear().apply();
+        swap_activity(read_from_pref());
+
+    }
+
+    private void swap_activity(String info) {
+
+        if (info != null) {
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            intent.putExtra("person_info", info);
+            startActivity(intent);
+        }
+
+    }
+
+    public String read_from_pref() {
+
+        String read_firstname = sharedPref.getString(key_first_name, "");
+        String read_surname = sharedPref.getString(key_last_name, "");
+        String read_address = sharedPref.getString(key_address, "");
+
+
+        if (!(read_address.equals("") || read_firstname.equals("") || read_surname.equals("")) ){
+            JSONArray json = new JSONArray();
+            json.put(read_firstname);
+            json.put(read_surname);
+            json.put(read_address);
+            return json.toString();
+        } else {
+            return null;
+        }
+
+
+    }
+
+
+    public void write_to_pref(View view) {
+
+        //write
+        Log.v("covid19", "save_btn_pressed");
+
+        TextView firstname = findViewById(R.id.et_name);
+        TextView lastname = findViewById(R.id.et_last_name);
+        TextView address = findViewById(R.id.et_address);
+
+        String firstname_text = firstname.getText().toString();
+        String lastname_text = lastname.getText().toString();
+        String address_text = address.getText().toString();
+
+        sharedPref.edit().putString(key_first_name, firstname_text).apply();
+        sharedPref.edit().putString(key_last_name, lastname_text).apply();
+        sharedPref.edit().putString(key_address, address_text).apply();
+
+
+
+
+        if (!(firstname_text.equals("") || lastname_text.equals("") || address_text.equals(""))) {
+            Log.v("covid19", "no empty values");
+            swap_activity(read_from_pref());
+        } else {
+            Log.v("covid19", "empty values");
+
+            Toast.makeText(this.getApplicationContext(), R.string.err_toast_info_missing_gr, Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+    //Getters and Setters
+    public String getKey_first_name() {
+        return key_first_name;
+    }
+
+    public String getKey_last_name() {
+        return key_last_name;
+    }
+
+    public String getKey_address() {
+        return key_address;
+    }
+
+
+}
--- app/src/main/java/com/example/covid19sms/MainActivity.java
+++ app/src/main/java/com/example/covid19sms/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.covid19sms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        String info = getIntent().getStringExtra("person_info");
+//
+//        Log.v("covid19", info);
+//
+//        info = info.replace("\"","");
+//        info = info.replace("[","");
+//        info = info.replace("]","");
+//
+//        String[] parts = info.split(",");
+
+
     }
+
 }
+
+
+
