bca96c411a08bf826a3e8307e9e7ed2e05ae9af2
Giorgos Rizikianos <rizikianosg@gmail.com>
2020-03-25 21:58:06 +0200

    fixed policyfragment & checkbox

--- app/src/main/java/com/SuncityDevs/covid19sms/PolicyFragment.java
+++ app/src/main/java/com/SuncityDevs/covid19sms/PolicyFragment.java
@@ -9,12 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 
 public class PolicyFragment extends Fragment {
 
     ViewPager viewPager;
     Button btn;
+    CheckBox checkBox;
+
     public PolicyFragment() {
         // Required empty public constructor
     }
@@ -33,22 +36,28 @@ public class PolicyFragment extends Fragment {
         // Inflate the layout for this fragment
 
 
-
         View view = inflater.inflate(R.layout.privacy_policy_layout, container, false);
         viewPager = getActivity().findViewById(R.id.viewPager);
-         btn = view.findViewById(R.id.privacy_policy_button);
-        btn.setOnClickListener(new View.OnClickListener(){
+        btn = view.findViewById(R.id.privacy_policy_button);
+        checkBox = view.findViewById(R.id.checkBox);
+        btn.setEnabled(false);
+        checkBox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((Tutorial) getActivity()).once_in_a_life_time();
-
-//                Intent intent = new Intent(getActivity(), Info_UI.class);
-//                Tutorial.first_time = "1";
-//
-//                startActivity(intent);
+                if(checkBox.isChecked()){
+                    btn.setEnabled(true);
+                    btn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            ((Tutorial)getActivity()).once_in_a_life_time();
+                        }
+                    });
+                }
+                else{
+                    btn.setEnabled(false);
+                }
             }
-
         });
-        return view;
+    return view;
     }
 }
